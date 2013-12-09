a7ac77566b525a3e4c006507f05c6160ca275138
Bruno Freitas <bmsatierf@gmail.com>
2013-12-09 14:58:28 -0200

    Using simpler sinatra file

--- merkaba.rb
+++ merkaba.rb
@@ -1,24 +1,12 @@
-#encoding: utf-8
-
-begin
-  # Require the preresolved locked set of gems
-  require ::File.expand_path('../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fallback on doing the resolve at runtime
-  require "rubygems"
-  require "bundler"
-  Bundler.setup
-end
-
 require 'rubygems'
 require 'sinatra'
 
 set :views, File.dirname(__FILE__) + '/templates'
 
-get '/indicadores' do
-  erb :indicadores
+get '/' do
+  erb :index
 end
 
 get '*' do
-  erb :index
+  redirect '/', 301
 end
